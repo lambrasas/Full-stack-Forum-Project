1e754e92a5c72bc3ab0cac038fa2e729a4df2c37
lambrasas <132943860+lambrasas@users.noreply.github.com>
2024-05-26 03:54:12 +0300

    Small changes

--- Frontend/src/components/EditableContent.jsx
+++ Frontend/src/components/EditableContent.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import InputField from "./InputField";
 const EditableContent = ({ content, onSave, threadId, maxLength = 1000 }) => {
